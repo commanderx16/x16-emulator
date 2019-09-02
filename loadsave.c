8885c8da733f4cffde6551a8db6d894a6a4feaa4
Michael Steil <mist64@mac.com>
2019-09-02 22:57:34 +0200

    LOAD"$" support in LOAD hypercall

--- loadsave.c
+++ loadsave.c
@@ -4,10 +4,111 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
 #include "glue.h"
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
+
+static int
+create_directory_listing(uint8_t *data)
+{
+	uint8_t *data_start = data;
+	struct stat st;
+	DIR *dirp;
+	struct dirent *dp;
+	int file_size;
+
+	// We inject this directly into RAM, so
+	// this does not include the load address!
+
+	// link
+	*data++ = 1;
+	*data++ = 1;
+	// line number
+	*data++ = 0;
+	*data++ = 0;
+	*data++ = 0x12; // REVERSE ON
+	*data++ = '"';
+	for (int i = 0; i < 16; i++) {
+		*data++ = ' ';
+	}
+	if (!(getcwd((char *)data - 16, 256))) {
+		return false;
+	}
+	*data++ = '"';
+	*data++ = ' ';
+	*data++ = '0';
+	*data++ = '0';
+	*data++ = ' ';
+	*data++ = 'P';
+	*data++ = 'C';
+	*data++ = 0;
+
+	if (!(dirp = opendir("."))) {
+		return 0;
+	}
+	while ((dp = readdir(dirp))) {
+		size_t namlen = strlen(dp->d_name);
+		stat(dp->d_name, &st);
+		file_size = (st.st_size + 255)/256;
+		if (file_size > 0xFFFF) {
+			file_size = 0xFFFF;
+		}
+
+		// link
+		*data++ = 1;
+		*data++ = 1;
+
+		*data++ = file_size & 0xFF;
+		*data++ = file_size >> 8;
+		if (file_size < 1000) {
+			*data++ = ' ';
+			if (file_size < 100) {
+				*data++ = ' ';
+				if (file_size < 10) {
+					*data++ = ' ';
+				}
+			}
+		}
+		*data++ = '"';
+		if (namlen > 16) {
+			namlen = 16; // TODO hack
+		}
+		memcpy(data, dp->d_name, namlen);
+		data += namlen;
+		*data++ = '"';
+		for (int i = namlen; i < 16; i++) {
+			*data++ = ' ';
+		}
+		*data++ = ' ';
+		*data++ = 'P';
+		*data++ = 'R';
+		*data++ = 'G';
+		*data++ = 0;
+	}
+
+	// link
+	*data++ = 1;
+	*data++ = 1;
+
+	*data++ = 255; // "65535"
+	*data++ = 255;
+
+	char *blocks_free = "BLOCKS FREE.";
+	memcpy(data, blocks_free, strlen(blocks_free));
+	data += strlen(blocks_free);
+	*data++ = 0;
+
+	// link
+	*data++ = 0;
+	*data++ = 0;
+	(void)closedir(dirp);
+	return data - data_start;
+}
 
 void
 LOAD()
@@ -19,31 +120,41 @@ LOAD()
 
 	uint16_t override_start = (x | (y << 8));
 
-	FILE *f = fopen(filename, "r");
-	if (!f) {
-		a = 4; // FNF
-		RAM[0x90] = a;
-		status |= 1;
-		return;
-	}
-	uint8_t start_lo = fgetc(f);
-	uint8_t start_hi = fgetc(f);
-	uint16_t start;
-	if (!RAM[0xb9]) {
-		start = override_start;
+	if (filename[0] == '$') {
+		uint16_t dir_len = create_directory_listing(RAM + override_start);
+		uint16_t end = override_start + dir_len;
+		x = end & 0xff;
+		y = end >> 8;
+		status &= 0xfe;
+		RAM[0x90] = 0;
+		a = 0;
 	} else {
-		start = start_hi << 8 | start_lo;
+		FILE *f = fopen(filename, "r");
+		if (!f) {
+			a = 4; // FNF
+			RAM[0x90] = a;
+			status |= 1;
+			return;
+		}
+		uint8_t start_lo = fgetc(f);
+		uint8_t start_hi = fgetc(f);
+		uint16_t start;
+		if (!RAM[0xb9]) {
+			start = override_start;
+		} else {
+			start = start_hi << 8 | start_lo;
+		}
+
+		size_t bytes_read = fread(RAM + start, 1, 0x9f00 - start, f);
+		fclose(f);
+
+		uint16_t end = start + bytes_read;
+		x = end & 0xff;
+		y = end >> 8;
+		status &= 0xfe;
+		RAM[0x90] = 0;
+		a = 0;
 	}
-
-	size_t bytes_read = fread(RAM + start, 1, 0x9f00 - start, f);
-	fclose(f);
-	uint16_t end = start + bytes_read;
-
-	x = end & 0xff;
-	y = end >> 8;
-	status &= 0xfe;
-	RAM[0x90] = 0;
-	a = 0;
 }
 
 void
