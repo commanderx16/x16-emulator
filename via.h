bc74003a7aaee7368999bc7893a13eae4c741d19
Michael Steil <mist64@mac.com>
2019-09-14 21:20:23 +0200

    faked VIA#1 timer registers to work around BASIC RND problem
    Fixes #48

--- via.h
+++ via.h
@@ -7,11 +7,13 @@
 
 #include <stdint.h>
 
+void via1_init();
 uint8_t via1_read(uint8_t reg);
 void via1_write(uint8_t reg, uint8_t value);
 uint8_t via2_read(uint8_t reg);
 void via2_write(uint8_t reg, uint8_t value);
 
+void via2_init();
 uint8_t via2_pb_get_out();
 void via2_pb_set_in(uint8_t value);
 void via2_sr_set(uint8_t value);
