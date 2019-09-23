e36afb3c72832d2073a90529db915123eb1b24b7
Mikael O. Bonnier <mobluse@users.noreply.github.com>
2019-09-23 02:09:58 +0200

    Update echochar.c

--- echochar.c
+++ echochar.c
@@ -2,19 +2,53 @@
 #include <stdint.h>
 
 void echochar(uint8_t c) {
-	switch (c) {
-		case 13: printf("\n"); break;
-		case 163: printf("£"); break;
-		case 164: printf("€"); break;
-		case 167: printf("§"); break;
-		case 172: printf("¬"); break;
-		case 181: printf("µ"); break;
-		case 196: printf("Ä"); break;
-		case 197: printf("Å"); break;
-		case 214: printf("Ö"); break;
-		case 228: printf("ä"); break;
-		case 229: printf("å"); break;
-		case 246: printf("ö"); break;
-		default: printf("%c", c);
+	static int mode = 0; /* 0: PETSCII, 1: ISO8859-15 */
+	static int shifted = 0; /* 0: Unshifted, 1: Shifted */
+	if (0x00 <= c && c <= 0x1F || 0x7F <= c && c <= 0x9F) {
+		switch (c) { 
+			case 0x0C: printf("\n"); break;
+			case 0x0E: shifted = 1; break;
+			case 0x0F: mode = 1; shifted = 1; break;
+			case 0x13: printf("\e[H"); break;
+			case 0x7F: printf(" "); break;
+			case 0x8E: shifted = 0; break;
+			case 0x8F: mode = 0; shifted = 0; break;
+			case 0x93: printf("\e[2J"); break;
+		}
+	} else {
+		switch (mode) {
+			case 0:
+				switch (shifted) {
+					case 0:
+						switch (c) {
+							case 126: printf("π"); break;
+							default: printf("%c", c);
+						}
+						break;
+					case 1:
+						switch (c) {
+							case 126: printf("▒"); break;
+							default: printf("%c", c);
+						}
+						break;
+				}
+				break;
+			case 1:
+				switch (c) {
+					case 163: printf("£"); break;
+					case 164: printf("€"); break;
+					case 167: printf("§"); break;
+					case 172: printf("¬"); break;
+					case 181: printf("µ"); break;
+					case 196: printf("Ä"); break;
+					case 197: printf("Å"); break;
+					case 214: printf("Ö"); break;
+					case 228: printf("ä"); break;
+					case 229: printf("å"); break;
+					case 246: printf("ö"); break;
+					default: printf("%c", c);
+				}
+				break;
+		}
 	}
 }
