a57d7b6809438b126a317ef82d608691fef96c98
Kobrasadetin <mikko.karjanmaa@gmail.com>
2019-11-15 23:46:38 +0200

    Optimized character printing in debugger (#210)

--- rendertext.h
+++ rendertext.h
@@ -9,6 +9,7 @@
 extern int xPos;
 extern int yPos;
 
+void DEBUGInitChars(SDL_Renderer *renderer);
 void DEBUGWrite(SDL_Renderer *renderer, int x, int y, int ch, SDL_Color colour);
 void DEBUGString(SDL_Renderer *renderer, int x, int y, char *s, SDL_Color colour);
 char *ltrim(char *s);
