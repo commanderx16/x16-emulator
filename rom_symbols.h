09b034f2f492ceb16d855556d6e3cd39d67b4691
Michael Steil <mist64@mac.com>
2020-01-03 20:47:38 +0100

    numeric argument for `-test`

--- rom_symbols.h
+++ rom_symbols.h
@@ -1,8 +1,8 @@
 #define NDX 0x00A00B
 #define KEYD 0x00A001
-#define FA 0x00028B
+#define FA 0x000291
 #define VARTAB 0x0003E3
-#define FNLEN 0x000288
+#define FNLEN 0x00028E
 #define FNADR 0x00008C
-#define STATUS 0x000280
-#define SA 0x00028A
+#define STATUS 0x000286
+#define SA 0x000290
