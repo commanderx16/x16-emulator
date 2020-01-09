fa36e1e0b0b778f0a58f6111f4e810d56bb02c84
mattuna15 <matthew.pearce@sky.uk>
2020-01-09 10:48:01 +0000

    editor config fixes

--- xcode/X16/iOS/JoystickController.swift
+++ xcode/X16/iOS/JoystickController.swift
@@ -1,8 +1,8 @@
 //
-//  JoystickController.swift
-//  CommanderX16
+//	JoystickController.swift
+//	CommanderX16
 //
-//  ; (C)2020 Matthew Pearce, License: 2-clause BSD
+//	; (C)2020 Matthew Pearce, License: 2-clause BSD
 //
 
 import Foundation
@@ -20,8 +20,8 @@ public class JoystickController: NSObject {
 	public var joyStick: JoyStickView?
 	var delegate: JoystickControllerDelegate?
 
-    let joystickOffset: CGFloat = 60.0
-    let joystickSpan: CGFloat = 80.0
+	let joystickOffset: CGFloat = 60.0
+	let joystickSpan: CGFloat = 80.0
 
 	@objc
 	public func getJoyStickFor(delegate: JoystickControllerDelegate) -> JoyStickView {
@@ -39,17 +39,17 @@ public class JoystickController: NSObject {
 	}
 
 	private func makeJoystick() -> JoyStickView {
-        let frame = CGRect(origin: CGPoint(x: 0.0, y: 0.0), size: CGSize(width: joystickSpan, height: joystickSpan))
-        let joystick = JoyStickView(frame: frame)
+		let frame = CGRect(origin: CGPoint(x: 0.0, y: 0.0), size: CGSize(width: joystickSpan, height: joystickSpan))
+		let joystick = JoyStickView(frame: frame)
 		joystick.backgroundColor = .clear
 		joystick.baseImage = UIImage(named: "FancyBase")
 		joystick.handleImage = UIImage(named: "FancyHandle")
 		joystick.movable = false;
-        joystick.alpha = 1
-        joystick.baseAlpha = 0.75
-        joystick.colorFillHandleImage = true
+		joystick.alpha = 1
+		joystick.baseAlpha = 0.75
+		joystick.colorFillHandleImage = true
 		joystick.travel = 0.5
-        return joystick
-    }
+		return joystick
+	}
 }
 
