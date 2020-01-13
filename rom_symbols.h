fd72426fa7e0253be5ac04b12f28c97f62f87ed8
Michael Steil <mist64@mac.com>
2020-01-13 08:33:32 +0100

    WIP

--- rom_symbols.h
+++ rom_symbols.h
@@ -1,8 +1,8 @@
 #define NDX 0x00A00A
 #define KEYD 0x00A000
-#define FA 0x000291
+#define FA 0x000290
 #define VARTAB 0x0003E3
-#define FNLEN 0x00028E
-#define FNADR 0x00008C
+#define FNLEN 0x00028D
+#define FNADR 0x00008A
 #define STATUS 0x000286
-#define SA 0x000290
+#define SA 0x00028F
