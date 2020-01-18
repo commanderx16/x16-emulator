30a5704402ecb6d2a6e522e910141fd3d0df5e0f
mattuna15 <matthew.pearce@sky.uk>
2020-01-18 11:44:30 +0000

    Update uartqueue.c
    License

--- socketuart/uartqueue.c
+++ socketuart/uartqueue.c
@@ -2,7 +2,7 @@
 //  uartqueue.c
 //  CommanderX16
 //
-//  Created by Pearce, Matthew (Senior Developer) on 16/01/2020.
+//	; (C)2020 Matthew Pearce, License: 2-clause BSD//
 //
 
 #include "uartqueue.h"
