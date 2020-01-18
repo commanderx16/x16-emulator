548b80fc7f1d9ca6e7d71f6f3b90d1dc685663c5
mattuna15 <matthew.pearce@sky.uk>
2020-01-18 11:44:59 +0000

    Update uartqueue.h

--- socketuart/uartqueue.h
+++ socketuart/uartqueue.h
@@ -2,7 +2,7 @@
 //  uartqueue.h
 //  CommanderX16
 //
-//  Created by Pearce, Matthew (Senior Developer) on 16/01/2020.
+//	; (C)2020 Matthew Pearce, License: 2-clause BSD//
 //
 
 #ifndef uartqueue_h
