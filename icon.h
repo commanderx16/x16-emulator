ad7f3f1b1784d328bd6a41615bcfa9f17376fdc0
Michael Steil <mist64@mac.com>
2020-03-22 13:18:56 +0100

    added window icon
    by Nigel Stewart <nigels@nigels.com>

---
+++ icon.h
@@ -0,0 +1,12 @@
+// Commander X16 Emulator
+// Copyright (c) 2019 Michael Steil
+// All rights reserved. License: 2-clause BSD
+
+#ifndef _ICON_H_
+#define _ICON_H_
+
+#include <SDL.h>
+
+SDL_Surface *CommanderX16Icon();
+
+#endif
