03f51529fb4af1f434e361c91422f7addb490d7d
Stephen Horn <indigo.darkwolf@gmail.com>
2020-07-09 02:55:15 -0500

    Adding VERA dump, fill commands to debugger (#279)

--- video.h
+++ video.h
@@ -25,4 +25,9 @@ void video_update_title(const char* window_title);
 uint8_t via1_read(uint8_t reg);
 void via1_write(uint8_t reg, uint8_t value);
 
+// For debugging purposes only:
+uint8_t video_space_read(uint32_t address);
+void video_space_write(uint32_t address, uint8_t value);
+
+
 #endif
