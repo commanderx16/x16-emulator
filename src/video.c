a8e546d5ce3e69549547847c6bad942bdd09950f
kktos <tm.barral@gmail.com>
2020-12-07 20:51:17 +0100

    Fix event handling to close app

--- src/video.c
+++ src/video.c
@@ -1098,6 +1098,9 @@ video_update()
 		if (event.type == SDL_QUIT) {
 			return false;
 		}
+		if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE) {
+			return false;
+		}
 		if (event.type == SDL_KEYDOWN) {
 			bool consumed = false;
 			if (cmd_down) {
