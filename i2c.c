1d575f41161afddd67405a77e97e7a5a1f8629a4
Michael Steil <mist64@mac.com>
2021-04-01 16:04:41 +0200

    WIP

--- i2c.c
+++ i2c.c
@@ -6,6 +6,8 @@
 #include <stdbool.h>
 #include "i2c.h"
 
+#define LOG_LEVEL 1
+
 i2c_port_t i2c_port;
 
 #define STATE_START 0
@@ -18,18 +20,40 @@ int count = 0;
 uint8_t device;
 uint8_t offset;
 
+uint8_t
+i2c_peek(uint8_t device, uint8_t offset) {
+    uint8_t value = 0xea;
+#if LOG_LEVEL >= 1
+    printf("I2C PEEK($%02X:$%02X) = $%02X\n", device, offset, value);
+#endif
+    return value;
+}
+
+void
+i2c_poke(uint8_t device, uint8_t offset, uint8_t value) {
+#if LOG_LEVEL >= 1
+    printf("I2C POKE $%02X:$%02X, $%02X\n", device, offset, byte);
+#endif
+}
+
 void
 i2c_step()
 {
     static i2c_port_t old_i2c_port;
     if (old_i2c_port.clk_in != i2c_port.clk_in || old_i2c_port.data_in != i2c_port.data_in) {
+#if LOG_LEVEL >= 5
         printf("I2C(%d) C:%d D:%d\n", state, i2c_port.clk_in, i2c_port.data_in);
+#endif
         if (state == STATE_STOP && i2c_port.clk_in == 0 && i2c_port.data_in == 0) {
+#if LOG_LEVEL >= 2
             printf("I2C START\n");
+#endif
             state = STATE_START;
         }
         if (state == 1 && i2c_port.clk_in == 1 &&i2c_port.data_in == 1 &&  old_i2c_port.data_in == 0) {
+#if LOG_LEVEL >= 2
             printf("I2C STOP\n");
+#endif
             state = STATE_STOP;
             count = 0;
             read_mode = false;
@@ -40,26 +64,33 @@ i2c_step()
             if (state < 8) {
                 if (read_mode) {
                     if (state == 0) {
-                        byte = 0xea; // XXX
-                        printf("I2C PEEK($%02X:$%02X) = $%02X\n", device, offset, byte);
+                        byte = i2c_peek(device, offset);
                     }
                     i2c_port.data_out = !!(byte & 0x80);
                     byte <<= 1;
+#if LOG_LEVEL >= 4
                     printf("I2C OUT#%d: %d\n", state, i2c_port.data_out);
+#endif
                     state++;
                 } else {
+#if LOG_LEVEL >= 4
                     printf("I2C BIT#%d: %d\n", state, i2c_port.data_in);
+#endif
                     byte <<= 1;
                     byte |= i2c_port.data_in;
                     state++;
                 }
             } else { // state == 8
                 if (read_mode) {
+#if LOG_LEVEL >= 3
                     printf("I2C OUT DONE\n");
-                    bool ack = i2c_port.data_in;
+#endif
+                    __unused bool ack = i2c_port.data_in;
                     offset++;
                 } else {
+#if LOG_LEVEL >= 3
                     printf("I2C ACK(%d) $%02X\n", count, byte);
+#endif
                     i2c_port.data_out = 0;
                     switch (count) {
                         case 0:
@@ -70,7 +101,7 @@ i2c_step()
                             offset = byte;
                             break;
                         default:
-                            printf("I2C POKE $%02X:$%02X, $%02X\n", device, offset, byte);
+                            i2c_poke(device, offset, byte);
                             offset++;
                             break;
                     }
