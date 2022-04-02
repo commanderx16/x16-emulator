6311d06a0a83faf0bf23054dc113a28eea34c1a0
Michael Steil <mist64@mac.com>
2022-04-02 19:26:01 +0200

    wip

--- src/serial.c
+++ src/serial.c
@@ -14,7 +14,8 @@ serial_step()
 	if (old_serial_port.atn_in != serial_port.atn_in ||
 		old_serial_port.clk_in != serial_port.clk_in ||
 		old_serial_port.data_in != serial_port.data_in) {
-		printf("SERIAL ATN:%d CLK:%d DATA:%d\n", serial_port.atn_in, serial_port.clk_in, serial_port.data_in);
+//		printf("-SERIAL ATN:%d CLK:%d DATA:%d\n", old_serial_port.atn_in, old_serial_port.clk_in, old_serial_port.data_in);
+		printf("+SERIAL ATN:%d CLK:%d DATA:%d\n", serial_port.atn_in, serial_port.clk_in, serial_port.data_in);
 		old_serial_port = serial_port;
 	}
 }
--- src/via.c
+++ src/via.c
@@ -246,8 +246,8 @@ via1_init()
 {
 	via_init(&via[0]);
 	i2c_port.clk_in = 1;
-	serial_port.clk_in = 0;
-	serial_port.data_in = 0;
+	serial_port.clk_in = 1;
+	serial_port.data_in = 1;
 }
 
 uint8_t
@@ -310,14 +310,15 @@ via1_write(uint8_t reg, uint8_t value)
 	if (reg == 0 || reg == 2) {
 		ps2_autostep(1);
 		i2c_step();
-		serial_step();
 		// PB
 		const uint8_t pb = via[0].registers[0] | ~via[0].registers[2];
 		ps2_port[1].in   = pb & PS2_VIA_MASK;
 		i2c_port.data_in = (pb & I2C_DATA_MASK) != 0;
+//		printf("!SERIAL ATN:%d CLK:%d DATA:%d\n", !!(pb & SERIAL_ATNIN_MASK), !!(pb & SERIAL_CLOCKIN_MASK), !!(pb & SERIAL_DATAIN_MASK));
 		serial_port.atn_in = (pb & SERIAL_ATNIN_MASK) != 0;
 		serial_port.clk_in = (pb & SERIAL_CLOCKIN_MASK) != 0;
 		serial_port.data_in = (pb & SERIAL_DATAIN_MASK) != 0;
+		serial_step();
 	} else if (reg == 1 || reg == 3) {
 		ps2_autostep(0);
 		// PA
