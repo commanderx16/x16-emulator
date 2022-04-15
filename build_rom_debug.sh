557b5b354d2a768a4bcea9b58d7b0c2d177dcdee
Michael Steil <mist64@mac.com>
2022-04-15 09:40:11 +0200

    build_rom_debug.sh: rebuild LST if it has changed

--- build_rom_debug.sh
+++ build_rom_debug.sh
@@ -9,7 +9,7 @@ cp ../x16-rom/build/x16/rom_labels.h src
 cp ../x16-rom/build/x16/rom_lst.h src
 
 # always force re-build of main.c because of updated rom_labels.h
-rm -f main.o
+rm -f build/main.o
 
 # build emulator with `-trace` support
 TRACE=1 make
